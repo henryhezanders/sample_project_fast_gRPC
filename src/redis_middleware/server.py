34d843b75a664112617732cfcad6ad8c1a3f4d07
Henry <h.he@zanders.eu>
2023-07-12 23:59:00 +0200

    chore(redis_middleware): add dockerfile

--- src/redis_middleware/server.py
+++ src/redis_middleware/server.py
@@ -1,11 +1,26 @@
 # gRPC microservice
 import hmac
-from redis_middleware.common.settings import settings
+from common.settings import settings
 import redis
 from concurrent import futures
 import grpc
 from proto import token_service_pb2
 from proto import token_service_pb2_grpc
+import logging
+
+# Create a handler
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+
+# Set the level of logging you want
+handler.setLevel(logging.INFO)
+
+# Set the logging format
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+
+# Add the handler to the logger
+logger.addHandler(handler)
 
 class TokenServiceServicer(token_service_pb2_grpc.TokenServiceServicer):
     def __init__(self):
@@ -13,6 +28,7 @@ class TokenServiceServicer(token_service_pb2_grpc.TokenServiceServicer):
 
     def StoreToken(self, request, context):
         # Create token
+        logger.info("storing token")
         token = ""
 
         # Store in Redis with expiration
@@ -21,6 +37,7 @@ class TokenServiceServicer(token_service_pb2_grpc.TokenServiceServicer):
         return token_service_pb2.StoreTokenResponse(success=True)
 
 def serve():
+    logger.info("starting server")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     token_service_pb2_grpc.add_TokenServiceServicer_to_server(TokenServiceServicer(), server)
     server.add_insecure_port('[::]:50051')
